42642201510e4777dd325659dded5b74afe87d65
Andrew Styles <andystyles@hotmail.co.uk>
2023-01-21 09:18:54 +0000

    changed HTML time blocks to be a bit simpler, added event listener and local storage to 9am text area

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,21 @@
-localStorage.setItem("9am", "no") //set the "9am" to the save button
-$("textarea").text(localStorage.getItem("9am")) 
+////set the "9am" to the save button
+//$("textarea").text(localStorage.getItem("9am"));
 
 // data-time="" attribute added to each text area and button
 // use moment to determine if current time is past/present/future and apply class to it
 // $("textarea").addClass("present")
 
 //use this to access text area closest to a button with a data-time
-$("button").on("click", function(event){
-
-    console.log($(event.target).siblings("textarea").val())
-    console.log($(event.target).attr("data-time"))
-
-})
-
 let currentDay = moment().format("dddd MMMM Do");
 let currentTime = moment().format("HH:mm")
 $("#currentDay").text(currentDay)
+
+$('#btn9').on('click',function(event) {
+
+    let $submitText = $(event.target).siblings('textarea').val()
+    localStorage.setItem("btn9",$submitText);
+ })
+
+ $("#text9").text(localStorage.getItem("btn9"));
+
+
